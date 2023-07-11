6c1d7faf9b95c8f3c1ad7db3bd22bec5fd168690
user <andreilkt@yandex.ru>
2023-07-11 18:32:53 +0700

    Initial commit

---
+++ test_class_sf.py
@@ -0,0 +1,7 @@
+class TestClassSF:
+    def test_one(self):
+        x = 1
+        assert x != 1 // False
+    def test_two(self):
+        x = 1
+        assert x != 2 // True
---
+++ welcome_test.py
@@ -0,0 +1,4 @@
+from welcome import hello
+def test_hello():
+    name = "SkillFactory"
+    assert hello(name) == "Hello, SkillFactory"
