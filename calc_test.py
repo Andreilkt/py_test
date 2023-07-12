8063a543cba458908c8c558d140d34bf1dd0ede3
andrei <andreilkt@yandex.ru>
2023-07-12 22:16:01 +0700

    Добавлена функция отрицательных чисел

--- calc_test.py
+++ calc_test.py
@@ -1,12 +1,21 @@
+import pytest
 
 from calc import sum
+
 
 def test_summ():
     first = 2
     second = 1
     assert sum(first, second) == 3
 
-def subtruct():
+
+def test_subtruct():
     first = 2
     second = 1
     assert sum(first, second) == 3
+
+
+def test_negative():
+    first = -2
+    second = -1
+    assert sum(first, second) == -3
